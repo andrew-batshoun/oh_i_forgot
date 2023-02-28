b7bb2a4477d0df47a1c210855dd399d7a03bf6d0
Batshoun <andrew.batshoun@accenturefederal.com>
2023-02-28 13:37:32 -0600

    update ui

--- src/app/tasks/task-list.component.ts
+++ src/app/tasks/task-list.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Task } from './task';
 import { TaskService } from './task.service';
+import {faPen} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-task-list',
-  templateUrl: './task-list.component.html',
+  templateUrl: './task-new-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
   errorMessage: string = "";
   sub: Subscription;
   newTask: Task;
+  faPen = faPen;
+
   
    
 
